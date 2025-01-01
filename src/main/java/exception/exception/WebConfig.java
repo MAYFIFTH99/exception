57062406d073691c80345dd1b41eff8bfa377f7d
harvey <alstjr971@naver.com>
2025-01-01 20:11:50 +0900

    HandlerExceptionResolver 활용 - ExceptionResolver에서 예외 처리 완료하기
    ExceptionResolver에서 예외를 처리하고, ModelAndView를 적절히 return하게
    되면 WAS로 예외가 넘어가지 않고 정상 반환되면서, 비효율적인 예외 로직
    호출을 생략할 수 있게된다.
    하지만, 너무 복잡하다.

--- src/main/java/exception/exception/WebConfig.java
+++ src/main/java/exception/exception/WebConfig.java
@@ -2,13 +2,17 @@ package exception.exception;
 
 import exception.exception.filter.LogFilter;
 import exception.exception.interceptor.LogInterceptor;
+import exception.exception.resolver.MyHandlerExceptionResolver;
+import exception.exception.resolver.UserHandlerExceptionResolver;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -29,5 +33,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");// 오류 페이지 경로
+    }
+
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserHandlerExceptionResolver());
     }
 }
